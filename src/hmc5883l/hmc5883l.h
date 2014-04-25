d9866544550db521d0137ed0df0fb3618196771b
Brendan Le Foll <brendan.le.foll@intel.com>
2014-04-25 14:48:43 +0100

    initial commit of upm with hmc5883l support
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

---
+++ src/hmc5883l/hmc5883l.h
@@ -0,0 +1,45 @@
+/*
+ * Author: Brendan Le Foll <brendan.le.foll@intel.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include <maa/maa.h>
+
+#define MAX_BUFFER_LENGTH 6
+
+namespace upm {
+
+class Hmc5883l {
+public:
+    Hmc5883l();
+    float direction();
+    float heading();
+    int* coordinates();
+    int update();
+private:
+    int coor[3];
+    char rx_tx_buf[MAX_BUFFER_LENGTH];
+    maa::I2CSlave* i2c;
+};
+
+}
