725fc4661c5dd168ba6be1bc6aadd1647ea1e1f1
Yevgeniy Kiveisha <yevgeniy.kiveisha@intel.com>
2014-05-28 05:56:44 -0700

    buzzer: added buzzer with example
    Signed-off-by: Yevgeniy Kiveisha <yevgeniy.kiveisha@intel.com>

---
+++ examples/buzzer-sound.cxx
@@ -0,0 +1,66 @@
+/*
+ * Author: Yevgeniy Kiveisha <yevgeniy.kiveisha@intel.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <iostream>
+#include <signal.h>
+
+#include "buzzer.h"
+
+int running = 0;
+
+void
+sig_handler(int signo)
+{
+	printf("got signal\n");
+    if (signo == SIGINT) {
+        printf("exiting application\n");
+        running = 1;
+    }
+}
+
+int
+main(int argc, char **argv) {
+	int chord[] = { DO, RE, MI, FA, SOL, LA, SI, DO, SI };
+	// create Buzzer instance
+    upm::Buzzer* sound = new upm::Buzzer(5);
+	// print sensor name
+    std::cout << sound->name() << std::endl;
+	// play sound (DO, RE, ME, etc...)
+
+	signal(SIGINT, sig_handler);
+
+	while (!running) {
+		for (int chord_ind = 0; chord_ind < 9; chord_ind++) {
+			std::cout << sound->playSound(chord[chord_ind]) << std::endl;
+			usleep(1000);
+		}
+	}
+
+	std::cout << "exiting application" << std::endl;
+
+	delete sound;
+    
+    return 0;
+}
---
+++ src/buzzer/buzzer.h
@@ -0,0 +1,53 @@
+/*
+ * Author: Yevgeniy Kiveisha <yevgeniy.kiveisha@intel.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include <string>
+#include <maa/pwm.h>
+
+#define  DO     3830000    // 261 Hz
+#define  RE     3400000    // 294 Hz
+#define  MI     3038000    // 329 Hz
+#define  FA     2864000    // 349 Hz
+#define  SOL    2550000    // 392 Hz
+#define  LA     2272000    // 440 Hz
+#define  SI     2028000    // 493 Hz
+
+namespace upm {
+
+class Buzzer {
+    public:
+		Buzzer (int pinNumber);
+		~Buzzer ();
+		int playSound (int note);
+        std::string name()
+        {
+            return m_name;
+        }
+    protected:
+        std::string m_name;
+	private:
+		maa_pwm_context m_pwm_context;
+};
+}
