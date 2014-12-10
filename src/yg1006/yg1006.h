3d5b54aa331ca97b8349ac11086a0fcd86479b80
Jon Trulson <jtrulson@ics.com>
2014-12-10 13:19:29 -0700

    yg1006: Initial implementation
    This module implements support for the yg1006 flame sensor.  It was
    tested with the Grove Flame Sensor.
    
    Signed-off-by: Jon Trulson <jtrulson@ics.com>
    Signed-off-by: Zion Orent <zorent@ics.com>

---
+++ src/yg1006/yg1006.h
@@ -0,0 +1,64 @@
+/*
+ * Author: Jon Trulson <jtrulson@ics.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include <string>
+#include <mraa/gpio.h>
+
+namespace upm {
+
+  /**
+   * @brief C++ API for the YG1006 flame sensor
+   *
+   * UPM module for the YG1006 flame sensor.  It detects flame or any
+   * other light in the 760nm - 1100nm range.
+   *
+   * @ingroup gpio
+   * @snippet yg1006.cxx Interesting
+   */
+  class YG1006 {
+  public:
+    /**
+     * YG1006 digital flame sensor constructor
+     *
+     * @param pin digital pin to use
+     */
+    YG1006(int pin);
+    /**
+     * YG1006 Destructor
+     */
+    ~YG1006();
+    /**
+     * Determine whether a flame has been detected
+     *
+     * @return True if a flame or other comparable light source is detected
+     */
+    bool flameDetected();
+
+  private:
+    mraa_gpio_context m_gpio;
+  };
+}
+
+
