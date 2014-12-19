7d4042539130f98a39dc7952a7f905f86cad0036
Zion Orent <zorent@ics.com>
2014-12-19 09:20:48 -0500

    ppd42ns: Initial implementation of the Grove dust sensor jrvandr: removed unnecessary mraa_init()
    Signed-off-by: Zion Orent <zorent@ics.com>
    Signed-off-by: Jon Trulson <jtrulson@ics.com>
    Signed-off-by: John Van Drasek <john.r.van.drasek@intel.com>

---
+++ src/ppd42ns/ppd42ns.h
@@ -0,0 +1,78 @@
+/*
+ * Author: Zion Orent <sorent@ics.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include <string>
+#include <time.h>
+#include <mraa/aio.h>
+
+typedef struct
+{
+	int lowPulseOccupancy;
+	double ratio;
+	double concentration;
+} dustData;
+
+namespace upm {
+
+  /**
+   * @brief C++ API for the Dust Sensor
+   *
+   * UPM module for the Dust Sensor
+   *
+   * @ingroup gpio
+   * @snippet ppd42ns.cxx Interesting
+   */
+  class PPD42NS {
+  public:
+    /**
+     * Dust Sensor Constructor
+     *
+     * @param pin digital pin to use
+     */
+    PPD42NS(int pin);
+    /**
+     * Dust Sensor Destructor
+     */
+    ~PPD42NS();
+    /**
+     * Print dust concentration
+     *
+     * @return struct dustData  contains data from dust sensor
+     */
+     dustData getData();
+
+  private:
+        mraa_gpio_context m_gpio;
+		/**
+		 * Return the amount of time it takes a pin to go from HIGH to LOW or LOW to HIGH
+		 *
+		 * @param highLowValue int do we measure from HIGH to LOW or LOW to HIGH. highLowValue is "from" value
+		 */
+		double pulseIn_polyfill(bool highLowValue, double endTime);
+		double m_timediff(timespec time1, timespec time2);
+	};
+}
+
+
