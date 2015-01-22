bae453d12eb5be35f36709f770652c2bed08594f
John Van Drasek <john.r.van.drasek@intel.com>
2015-01-22 13:36:32 -0800

    nrf8001: Changing comment style to /**< comment */
    Signed-off-by: John Van Drasek <john.r.van.drasek@intel.com>

--- src/nrf8001/boards.h
+++ src/nrf8001/boards.h
@@ -26,9 +26,9 @@
 #ifndef _BLE_BOARDS_H_
 #define _BLE_BOARDS_H_
 
-#define BOARD_DEFAULT               0 //!< Use this if you do not know the board you are using or you are creating a new one
-#define REDBEARLAB_SHIELD_V1_1      1 //!< Redbearlab Bluetooth low energy shield v1.1
-#define REDBEARLAB_SHIELD_V2012_07  1 //!< Identical to Redbearlab v1.1 shield
-#define REDBEARLAB_SHIELD_V2        0 //!< Redbearlab Bluetooth low energy shield v2.x - No special handling required for pin reset same as default
+#define BOARD_DEFAULT               0 /**< Use this if you do not know the board you are using or you are creating a new one */
+#define REDBEARLAB_SHIELD_V1_1      1 /**< Redbearlab Bluetooth low energy shield v1.1 */
+#define REDBEARLAB_SHIELD_V2012_07  1 /**< Identical to Redbearlab v1.1 shield */
+#define REDBEARLAB_SHIELD_V2        0 /**< Redbearlab Bluetooth low energy shield v2.x - No special handling required for pin reset same as default */
 
 #endif
