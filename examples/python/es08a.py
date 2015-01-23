09659c62f37e4839862d7f66dad723837ee1f0a8
Sarah Knepper <sarah.knepper@intel.com>
2015-01-22 20:11:10 -0800

    es08a: Add output to python example
    Signed-off-by: Sarah Knepper <sarah.knepper@intel.com>

--- examples/python/es08a.py
+++ examples/python/es08a.py
@@ -29,14 +29,17 @@ gServo = servo.ES08A(5)
 for i in range(0,10): 
     # Set the servo arm to 0 degrees
     gServo.setAngle(0)
+    print 'Set angle to 0'
     time.sleep(1)
 
     # Set the servo arm to 90 degrees
     gServo.setAngle(90)
+    print 'Set angle to 90'
     time.sleep(1)
 
     # Set the servo arm to 180 degrees
     gServo.setAngle(180)
+    print 'Set angle to 180'
     time.sleep(1)
 
 # Delete the servo object
