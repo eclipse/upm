925260a23464719ebdcd212bd4e599b320a3a7ff
Zion Orent <zorent@ics.com>
2015-02-25 17:54:57 -0500

    ina132: initial implementation
    This module was tested with the Grove Differential Amplifier, with a
    weight sensor attached.
    
    Signed-off-by: Zion Orent <zorent@ics.com>
    Signed-off-by: Jon Trulson <jtrulson@ics.com>
    Signed-off-by: John Van Drasek <john.r.van.drasek@intel.com>

---
+++ src/ina132/ina132.h
@@ -0,0 +1,71 @@
+/*
+ * Author: Zion Orent <zorent@ics.com>
+ * Copyright (c) 2015 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include <string>
+#include <mraa/aio.h>
+
+namespace upm {
+  /**
+   * @brief UPM module for the INA132 Differential Amplifier Sensor
+   * @defgroup ina132 libupm-ina132
+   */
+  /**
+   * @brief C++ API for the INA132 Differential Amplifier Sensor
+   * 
+   * The INA132 Differential Amplifier Sensor is designed
+   * for precise differential-input amplification.
+   * 
+   * This sensor was tested amplifying the signal
+   * from a Weight Sensor (Load Cell)0-500g
+   *
+   * @ingroup analog ina132
+   * @snippet ina132.cxx Interesting
+   */
+  class INA132 {
+  public:
+    /**
+     * INA132 sensor constructor
+     *
+     * @param pin analog pin to use
+     */
+    INA132(int pin);
+    /**
+     * INA132 Destructor
+     */
+    ~INA132();
+
+    /**
+     * Measure precise differential-input amplification from the sensor
+     *
+     * @return precise differential-input amplification
+     */
+    float value();
+
+  private:
+    mraa_aio_context m_aio;
+  };
+}
+
+
