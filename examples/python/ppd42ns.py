22779337c81d1997edc37817ed3b14cbd860439b
Zion Orent <zorent@ics.com>
2015-03-23 13:55:50 -0400

    ppd42ns: python example for ppd42ns dust sensor
    Signed-off-by: Zion Orent <zorent@ics.com>
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

---
+++ examples/python/ppd42ns.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# Author: Zion Orent <zorent@ics.com>
+# Copyright (c) 2015 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import time, sys, signal, atexit
+import pyupm_ppd42ns as upmPpd42ns
+
+# Instantiate a dust sensor on digital pin D8
+myDustSensor = upmPpd42ns.PPD42NS(8)
+
+
+## Exit handlers ##
+# This function stops python from printing a stacktrace when you hit control-C
+def SIGINTHandler(signum, frame):
+	raise SystemExit
+
+# This function lets you run code on exit, including functions from myDustSensor
+def exitHandler():
+	print "Exiting"
+	sys.exit(0)
+
+# Register exit handlers
+atexit.register(exitHandler)
+signal.signal(signal.SIGINT, SIGINTHandler)
+
+
+notice = ("This program will give readings "
+"every 30 seconds until you stop it")
+print notice
+
+while(1):
+	data = myDustSensor.getData()
+	# we need to sleep for a bit for the data to print out
+	time.sleep(.1)
+	print "Low pulse occupancy: " + str(data.lowPulseOccupancy)
+	print "Ratio: " + str(data.ratio)
+	print "Concentration: " + str(data.concentration)
