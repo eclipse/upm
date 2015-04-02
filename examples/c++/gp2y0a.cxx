77be6adf6de42d642c626b80b286359ae11423c1
Jon Trulson <jtrulson@ics.com>
2015-04-02 11:03:29 -0600

    gp2y0a: renamed from gp2y0a21yk to indicate support for the gp2y0a family
    Additionally, add some error checking in the constructor, and determine
    the ADC resolution and use that in the voltage calculation rather than
    a hardcoded 1024.0.
    
    Signed-off-by: Jon Trulson <jtrulson@ics.com>
    Signed-off-by: Zion Orent <zorent@ics.com>
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- examples/c++/gp2y0a.cxx
+++ examples/c++/gp2y0a.cxx
@@ -25,14 +25,14 @@
 #include <unistd.h>
 #include <iostream>
 #include <signal.h>
-#include "gp2y0a21yk.h"
+#include "gp2y0a.h"
 
 using namespace std;
 
 bool shouldRun = true;
 
 // analog voltage, usually 3.3 or 5.0
-#define GP2Y0A21YK_AREF   5.0
+#define GP2Y0A_AREF   5.0
 #define SAMPLES_PER_QUERY 20
 
 void sig_handler(int signo)
@@ -46,21 +46,21 @@ int main()
   signal(SIGINT, sig_handler);
 
 //! [Interesting]
-  // Note, for the Grove version of this sensor, due to the way it is wired,
-  // you need to plug this into the A0 port, where it will use the available
-  // A1 pin for data.
+  // Note, for the Grove 80cm version of this sensor, due to the way
+  // it is wired, you need to plug this into the A0 port, where it
+  // will use the available A1 pin for data.
 
-  // Instantiate a GP2Y0A21YK on analog pin A1
-  upm::GP2Y0A21YK *volts = new upm::GP2Y0A21YK(1);
+  // Instantiate a GP2Y0A on analog pin A1
+  upm::GP2Y0A *volts = new upm::GP2Y0A(1);
   
   // The higher the voltage (closer to AREF) the closer the object is.  NOTE:
   // the measured voltage will probably not exceed 3.3 volts.
   // Every second, print the averaged voltage value (averaged over 20 samples).
   while (shouldRun)
     {
-      cout << "AREF: " << GP2Y0A21YK_AREF 
+      cout << "AREF: " << GP2Y0A_AREF 
            << ", Voltage value (higher means closer): " 
-           << volts->value(GP2Y0A21YK_AREF, SAMPLES_PER_QUERY) << endl;
+           << volts->value(GP2Y0A_AREF, SAMPLES_PER_QUERY) << endl;
       
       sleep(1);
     }
