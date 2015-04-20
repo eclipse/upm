ed8cc6b21413d690dda81b49f5e29d17ec1ad92d
Scott Ware <scott2ware@scott-dev.(none)>
2015-04-20 13:57:29 +0100

    iADC: Add convert to volts function.

--- src/upm/iADC.h
+++ src/upm/iADC.h
@@ -9,6 +9,7 @@ namespace upm
    {
    public:
        virtual mraa_result_t getValue(int input, uint16_t *value) = 0;
+       virtual float convertToVolts(uint16_t value) = 0;
        virtual bool isConfigured() = 0;
        virtual ~IADC() {}
    };
