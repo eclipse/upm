ca6c6117740cc602944cdfb4c712136e2f370757
Brendan Le Foll <brendan.le.foll@intel.com>
2015-06-02 14:41:13 +0100

    uart: result of mraa_uart_get_dev_path is a const
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- src/mhz16/mhz16.cxx
+++ src/mhz16/mhz16.cxx
@@ -42,7 +42,7 @@ MHZ16::MHZ16(int uart)
     }
 
   // This requires a recent MRAA (1/2015)
-  char *devPath = mraa_uart_get_dev_path(m_uart);
+  const char *devPath = mraa_uart_get_dev_path(m_uart);
 
   if (!devPath)
     {
