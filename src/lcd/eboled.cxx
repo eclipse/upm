950477261c8553ef3921bcdc5288925d6107dacb
tylergibson <tgibson@microsoft.com>
2015-08-25 00:24:13 -0700

    Removing dupe method
    Signed-off-by: Tyler Gibson <tgibson@microsoft.com>

--- src/lcd/eboled.cxx
+++ src/lcd/eboled.cxx
@@ -187,48 +187,6 @@ void EBOLED::drawPixel(uint8_t x, uint8_t y, uint8_t color)
 }
 
 void EBOLED::drawLine(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t color)
-{
-        
-    int16_t steep = abs(y1 - y0) > abs(x1 - x0);
-        
-    if (steep) {
-        swap(x0, y0);
-        swap(x1, y1);
-    }
-
-    if (x0 > x1) {
-        swap(x0, x1);
-        swap(y0, y1);
-    }
-
-    int16_t dx, dy;
-    dx = x1 - x0;
-    dy = abs (y1 - y0);
-
-    int16_t err = dx / 2;
-    int16_t ystep;
-
-    if (y0 < y1) {
-        ystep = 1;
-    } else {
-        ystep = -1;
-    }
-
-    for (; x0 <= x1; x0++) {
-        if (steep) {
-            drawPixel(y0, x0, color);
-        } else {
-            drawPixel(x0, y0, color);
-        }
-        err -= dy;
-        if (err < 0) {
-            y0 += ystep;
-            err += dx;
-        }
-    }
-}
-
-void EBOLED::drawLine(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t color)
 {  
   int16_t steep = abs(y1 - y0) > abs(x1 - x0);
       
