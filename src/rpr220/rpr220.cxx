a2855c5e7294332a76e9d57850fa87d4a1035558
Jon Trulson <jtrulson@ics.com>
2015-09-08 12:54:07 -0600

    rpr220: throw exception(s) on fatal errors
    Signed-off-by: Jon Trulson <jtrulson@ics.com>
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- src/rpr220/rpr220.cxx
+++ src/rpr220/rpr220.cxx
@@ -23,6 +23,8 @@
  */
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 #include "rpr220.h"
 
@@ -35,7 +37,8 @@ RPR220::RPR220(int pin)
 
   if ( !(m_gpio = mraa_gpio_init(pin)) )
    {
-      cerr << __FUNCTION__ << ": mraa_gpio_init() failed" << endl;
+      throw std::invalid_argument(std::string(__FUNCTION__) +
+                                  ": mraa_gpio_init() failed, invalid pin?");
       return;
     }
 
