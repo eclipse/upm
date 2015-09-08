580a55bb2b58bb78604a05cf05abe2a248b0020d
Jon Trulson <jtrulson@ics.com>
2015-09-08 17:09:22 -0600

    rfr359f: throw exception(s) on fatal errors
    Signed-off-by: Jon Trulson <jtrulson@ics.com>

--- src/rfr359f/rfr359f.cxx
+++ src/rfr359f/rfr359f.cxx
@@ -23,6 +23,8 @@
  */
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 #include "rfr359f.h"
 
@@ -33,7 +35,8 @@ RFR359F::RFR359F(int pin)
 {
   if ( !(m_gpio = mraa_gpio_init(pin)) )
    {
-      cerr << __FUNCTION__ << ": mraa_gpio_init() failed" << endl;
+      throw std::invalid_argument(std::string(__FUNCTION__) +
+                                  ": mraa_gpio_init() failed, invalid pin?");
       return;
     }
 
