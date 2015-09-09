d7b8fac41b7f4a67ef6ee57eff0ac0be919815d9
Jon Trulson <jtrulson@ics.com>
2015-09-09 16:43:42 -0600

    grovecircularled: throw exception(s) on fatal errors
    Signed-off-by: Jon Trulson <jtrulson@ics.com>
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- src/grovecircularled/grovecircularled.cxx
+++ src/grovecircularled/grovecircularled.cxx
@@ -26,6 +26,8 @@
  */
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -37,6 +39,23 @@ GroveCircularLED::GroveCircularLED (uint8_t di, uint8_t dcki)
 									: m_clkPinCtx(dcki),
 									  m_dataPinCtx(di) {
   mraa::Result error = mraa::SUCCESS;
+  
+  // init clock context
+  m_clkPinCtx = mraa_gpio_init(dcki);
+  if (m_clkPinCtx == NULL) {
+    throw std::invalid_argument(std::string(__FUNCTION__) +
+                                ": mraa_gpio_init(dcki) failed, invalid pin?");
+    return;
+  }
+  mraa_gpio_use_mmaped(m_clkPinCtx, 1);
+
+  // init data context
+  m_dataPinCtx = mraa_gpio_init(di);
+  if (m_dataPinCtx == NULL) {
+    throw std::invalid_argument(std::string(__FUNCTION__) +
+                                ": mraa_gpio_init(di) failed, invalid pin?");
+    return;
+  }
 
 	m_clkPinCtx.useMmap(true);
 	m_dataPinCtx.useMmap(true);
