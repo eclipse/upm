4513a8ad092a072b81817614bc4837ddc7fc98a1
Jon Trulson <jtrulson@ics.com>
2015-09-10 10:56:07 -0600

    groveelectromagnet: throw exception(s) on fatal errors
    Signed-off-by: Jon Trulson <jtrulson@ics.com>
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- src/groveelectromagnet/groveelectromagnet.cxx
+++ src/groveelectromagnet/groveelectromagnet.cxx
@@ -22,13 +22,21 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <string>
+#include <stdexcept>
+
 #include "groveelectromagnet.h"
 
 using namespace upm;
 
 GroveElectromagnet::GroveElectromagnet(int pin)
 {
-    m_gpio = mraa_gpio_init(pin);
+    if ( !(m_gpio = mraa_gpio_init(pin)) ) 
+      {
+        throw std::invalid_argument(std::string(__FUNCTION__) +
+                                    ": mraa_gpio_init() failed, invalid pin?");
+        return;
+      }
     mraa_gpio_dir(m_gpio, MRAA_GPIO_OUT);
 }
 
