e16df918c4a4d1dccd5fbea506b51e5c57171b0d
Jon Trulson <jtrulson@ics.com>
2015-09-21 14:01:48 -0600

    lm35: Initial implementation
    Signed-off-by: Jon Trulson <jtrulson@ics.com>

---
+++ examples/javascript/lm35.js
@@ -0,0 +1,51 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+
+/*
+ * Author: Jon Trulson <jtrulson@ics.com>
+ * Copyright (c) 2015 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+var sensorObj = require('jsupm_lm35');
+
+// Instantiate a LM35 on analog pin A0, with a default analog
+// reference voltage of 5.0
+var sensor = new sensorObj.LM35(0);
+
+// Every half second, sample the sensor and output the temperature
+
+setInterval(function()
+{
+    console.log("Temperature: " + sensor.getTemperature() + " C");
+}, 500);
+
+// exit on ^C
+process.on('SIGINT', function()
+{
+    sensor = null;
+    sensorObj.cleanUp();
+    sensorObj = null;
+    console.log("Exiting.");
+    process.exit(0);
+});
+
---
+++ src/lm35/javaupm_lm35.i
@@ -0,0 +1,8 @@
+%module javaupm_lm35
+%include "../upm.i"
+
+%{
+    #include "lm35.h"
+%}
+
+%include "lm35.h"
