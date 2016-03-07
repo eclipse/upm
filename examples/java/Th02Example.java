75e42c6731c5453e73f2d224da0360ceb733e8d3
Stefan Andritoiu <stefan.andritoiu@intel.com>
2016-03-07 19:56:52 +0200

    java: eliminated loadlibrary code from examples
    Signed-off-by: Stefan Andritoiu <stefan.andritoiu@intel.com>
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- examples/java/Th02Example.java
+++ examples/java/Th02Example.java
@@ -25,18 +25,6 @@ import upm_th02.TH02;
 
 public class Th02Example {
 
-	static {
-		try {
-			System.loadLibrary("javaupm_th02");
-			System.loadLibrary("mraajava");
-		} catch (UnsatisfiedLinkError e) {
-			System.err.println(
-					"Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" +
-							e);
-			System.exit(1);
-		}
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		float temperature = 0;
