893b35f77b1b835e6e4c3c7bd7a9040bf6fbc203
Abhishek Malik <abhishek.malik@intel.com>
2016-04-29 11:07:02 -0700

    Java: Added Examples for Sensors
    max31855, max5487, maxds3231m, ecs1030 and sm130
    
    Signed-off-by: Abhishek Malik <abhishek.malik@intel.com>

---
+++ examples/java/MAX31855Example.java
@@ -0,0 +1,56 @@
+/*
+ * Author: Abhishek Malik <abhishek.malik@intel.com>
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+import upm_max31855.MAX31855;
+
+public class MAX31855Example {
+
+	static {
+		try {
+			System.loadLibrary("javaupm_max31855");
+			System.loadLibrary("mraajava");
+		} catch (UnsatisfiedLinkError e) {
+			System.err.println(
+					"Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" +
+							e);
+			System.exit(1);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//! [Interesting]
+		MAX31855 sensor = new MAX31855(0, 8);
+		while(true){
+			System.out.println("Temperature: "+sensor.getTemp());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("The following exception occured: "+e.getMessage());
+			}
+		}
+		//! [Interesting]
+	}
+
+}
