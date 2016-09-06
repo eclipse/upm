2b6dd5f3fda0b736fa4844e5081a71735c3019b9
Noel Eck <noel.eck@intel.com>
2016-09-05 21:57:41 -0700

    cmake: Cleanup of cmake messages.
        * Removed unessesary spaces.
        * Changed INFO to STATUS since INFO is not a cmake message
          keyword.
        * Changed a few messages from status/info to warning.
        * Fixed trailing carriage return on yuidoc execute_process.
        * Removed my debug messages for CXX vs C.
    
    Signed-off-by: Noel Eck <noel.eck@intel.com>

--- cmake/modules/FindYuidoc.cmake
+++ cmake/modules/FindYuidoc.cmake
@@ -14,9 +14,11 @@ find_package_handle_standard_args (Yuidoc DEFAULT_MSG
 # Get Yuidoc version
 if (YUIDOC_EXECUTABLE)
     execute_process(COMMAND ${YUIDOC_EXECUTABLE} --version
-                    ERROR_VARIABLE YUIDOC_VERSION)
+                    ERROR_VARIABLE YUIDOC_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    ERROR_STRIP_TRAILING_WHITESPACE)
     if(YUIDOC_VERSION)
-      message ("INFO - Yuidoc version is " ${YUIDOC_VERSION})
+      message (STATUS "Yuidoc version is ${YUIDOC_VERSION}")
     endif()
 endif ()
 
