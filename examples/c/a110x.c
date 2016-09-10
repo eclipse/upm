d548fc62abc7beff3f57d7c0798a3580fe97350c
Abhishek Malik <abhishek.malik@intel.com>
2016-09-09 19:19:35 -0700

    Examples: Added C Examples
    Added examples for:
    mq303a
    m24lr64e
    mpr121
    servo
    a110x
    gp2y0a
    ttp223
    loudness
    tsl2561
    
    Signed-off-by: Abhishek Malik <abhishek.malik@intel.com>

---
+++ examples/c/a110x.c
@@ -0,0 +1,23 @@
+//Modified: Abhishek Malik <abhishek.malik@intel.com>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "a110x.h"
+
+void main(void)
+{
+	a110x_context dev = a110x_init(2);
+	bool abc = 0;
+	while(1){
+		if(a110x_magnet_detected(dev, &abc) != UPM_SUCCESS){
+			printf("an error has occured\n");
+		}
+		upm_delay(1);
+		printf("value retrieved: %d\n", abc);
+	}
+
+	return 0;
+}
