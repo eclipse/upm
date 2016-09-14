4b6f8ecdd79e3c3189742ca2121bdd4926a4c878
Noel Eck <noel.eck@intel.com>
2016-09-14 11:51:17 -0700

    ultrasonic: Created ultrasonic from groveultrasonic.
        * Added C++ and javascript example.
        * Added jpg.
    
    Signed-off-by: Noel Eck <noel.eck@intel.com>

---
+++ examples/c++/ultrasonic.cxx
@@ -0,0 +1,60 @@
+/*
+ * Author: Jun Kato <i@junkato.jp>
+ * Copyright (c) 2015 Jun Kato.
+ *
+ * Thanks to Seeed Studio for a working arduino sketch
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <iostream>
+#include "ultrasonic.hpp"
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+upm::UltraSonic *sonar = NULL;
+bool running = true;
+
+void sig_handler(int signo)
+{
+    if (signo == SIGINT)
+        running = false;
+}
+
+int main(int argc, char **argv)
+{
+    signal(SIGINT, sig_handler);
+    //! [Interesting]
+    // upm::UltraSonic *sonar = NULL;
+    sonar = new upm::UltraSonic(2);
+    while(running)
+    {
+        int width = sonar->getDistance();
+        printf("Echo width = %d\n", width);
+        printf("Distance inches = %f.2\n\n", width/148.0);
+        sleep(3);
+    }
+    //! [Interesting]
+    printf("exiting application\n");
+    delete sonar;
+    return 0;
+}
