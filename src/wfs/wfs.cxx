b65908ea2420ab8b4637a4e8a4c3cdb187f5919d
Sisinty Sasmita Patra <sisinty.s.patra@intel.com>
2016-09-14 11:37:08 -0700

    WFS: Added water flow sensor without grove dependency
    Signed-off-by: Sisinty Sasmita Patra <sisinty.s.patra@intel.com>

---
+++ src/wfs/wfs.cxx
@@ -0,0 +1,124 @@
+/*
+ * Author: Jon Trulson <jtrulson@ics.com>
+ * Copyright (c) 2015 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+#include "wfs.hpp"
+
+using namespace upm;
+using namespace std;
+
+WFS::WFS(int pin)
+{
+  if ( !(m_gpio = mraa_gpio_init(pin)) )
+    {
+      throw std::invalid_argument(std::string(__FUNCTION__) +
+                                  ": mraa_gpio_init() failed, invalid pin?");
+      return;
+    }
+
+  mraa_gpio_dir(m_gpio, MRAA_GPIO_IN);
+
+  initClock();
+  m_flowCounter = 0;
+  m_isrInstalled = false;
+}
+
+WFS::~WFS()
+{
+  if (m_isrInstalled)
+    stopFlowCounter();
+
+  mraa_gpio_close(m_gpio);
+}
+
+void WFS::initClock()
+{
+  gettimeofday(&m_startTime, NULL);
+}
+
+uint32_t WFS::getMillis()
+{
+  struct timeval elapsed, now;
+  uint32_t elapse;
+
+  // get current time
+  gettimeofday(&now, NULL);
+
+  // compute the delta since m_startTime
+  if( (elapsed.tv_usec = now.tv_usec - m_startTime.tv_usec) < 0 ) 
+    {
+      elapsed.tv_usec += 1000000;
+      elapsed.tv_sec = now.tv_sec - m_startTime.tv_sec - 1;
+    } 
+  else 
+    {
+      elapsed.tv_sec = now.tv_sec - m_startTime.tv_sec;
+    }
+
+  elapse = (uint32_t)((elapsed.tv_sec * 1000) + (elapsed.tv_usec / 1000));
+
+  // never return 0
+  if (elapse == 0)
+    elapse = 1;
+
+  return elapse;
+}
+
+void WFS::startFlowCounter()
+{
+  initClock();
+  // install our interrupt handler
+  mraa_gpio_isr(m_gpio, MRAA_GPIO_EDGE_RISING, 
+                &flowISR, this);
+
+  m_isrInstalled = true;
+}
+
+void WFS::stopFlowCounter()
+{
+  // remove the interrupt handler
+  mraa_gpio_isr_exit(m_gpio);
+
+  m_isrInstalled = false;
+}
+
+void WFS::flowISR(void *ctx)
+{
+  upm::WFS *This = (upm::WFS *)ctx;
+  This->m_flowCounter++;
+}
+
+float WFS::flowRate()
+{
+  uint32_t millis = getMillis();
+  uint32_t flow = flowCounter();
+  
+  // 7.5 comes from the seeedstudio page, see the confusing datasheet :)
+  float flowRate = (float(flow) * 7.5) / ((float(millis) / 1000.0) * 60.0);
+
+  return flowRate;
+}
