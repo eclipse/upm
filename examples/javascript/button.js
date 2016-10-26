7bb8607b9b483780227754ffd6eb54a1f8a6a4a4
Suyash Lakhotia <suyashlakhotia@gmail.com>
2016-10-26 14:26:05 +0800

    Incorrect Function Name
    groveSensor.Button() --> groveSensor.GroveButton()

--- examples/javascript/button.js
+++ examples/javascript/button.js
@@ -26,7 +26,7 @@
 var groveSensor = require('jsupm_grove');
 
 // Create the button object using GPIO pin 0
-var button = new groveSensor.Button(0);
+var button = new groveSensor.GroveButton(0);
 
 // Read the input and print, waiting one second between readings
 function readButtonValue() {
